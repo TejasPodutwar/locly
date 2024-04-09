70d7a8d8695ea22a3cdd64793f37569bdbb7bb01
Sparsh Kapoor <sparsh.1923ec1133@kiet.edu>
2024-04-09 20:36:33 +0530

    workflow test

---
+++ demo/src/main/java/com/example/demo/TestWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWorkflowApplication.class, args);
+	}
+
+}
